382fa91ad408c93ab40104d6184319f5f92567d0
gr-im <grimfw@gmail.com>
2025-08-14 08:51:03 +0200

    Stewarding + Highlighting

--- lib/rule.ml
+++ lib/rule.ml
@@ -1,12 +1,24 @@
 open Yocaml
 
-let track_binary = Pipeline.track_file (Path.rel [ Sys.argv.(0) ])
+let track_binary = Pipeline.track_file (Path.from_string Sys.argv.(0))
+
+let from_markdown () =
+  let open Yocaml.Task in
+  Static.on_content
+    (Yocaml_cmarkit.to_doc ()
+    >>| Hilite_markdown.transform ~skip_unknown_languages:true
+    >>> Yocaml_cmarkit.from_doc_to_html ())
 
 let css ~target =
   let target = Resolver.css ~target in
   Action.Static.write_file target
   @@ Pipeline.pipe_files ~separator:"\n"
-       Path.[ rel [ "css"; "reset.css" ]; rel [ "css"; "style.css" ] ]
+       Path.
+         [
+           rel [ "css"; "reset.css" ]
+         ; rel [ "css"; "syntax.css" ]
+         ; rel [ "css"; "style.css" ]
+         ]
 
 let page ~target file =
   let target = Resolver.page ~target file in
@@ -14,7 +26,7 @@ let page ~target file =
     (let open Task in
      track_binary
      >>> Yocaml_yaml.Pipeline.read_file_with_metadata (module Repr.Page) file
-     >>> Yocaml_cmarkit.content_to_html ()
+     >>> from_markdown ()
      >>> Yocaml_jingoo.Pipeline.as_template
            (module Repr.Page)
            (Path.rel [ "templates"; "main.html" ]))
@@ -26,7 +38,7 @@ let article ~target file =
      track_binary
      >>> Yocaml_yaml.Pipeline.read_file_with_metadata (module Repr.Article) file
      >>> Repr.Article.prepare
-     >>> Yocaml_cmarkit.content_to_html ()
+     >>> from_markdown ()
      >>> Yocaml_jingoo.Pipeline.as_template
            (module Repr.Article)
            (Path.rel [ "templates"; "article.html" ])
@@ -61,7 +73,7 @@ let index ~target =
            (module Repr.Page)
            (Path.rel [ "index.md" ])
      >>> first (Repr.Articles.index articles)
-     >>> Yocaml_cmarkit.content_to_html ()
+     >>> from_markdown ()
      >>> Yocaml_jingoo.Pipeline.as_template
            (module Repr.Articles)
            (Path.rel [ "templates"; "articles.html" ])
